87136d3651b42f4d288275530738446bea608310
massifcoder <vishalsharma243527@gmail.com>
2023-06-05 15:06:34 +0530

    add meet in options

--- components/word/files/SideBar.js
+++ components/word/files/SideBar.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function SideBar(){
     return(
-        <div className="w-1/5 p-4 text-center h-80 flex flex-col space-y-4 pt-8">
+        <div className="w-1/5 p-4 select-none text-center h-80 flex flex-col space-y-4 pt-8">
             <Link href={'/word'}>
                 <div className="bg-blue-500 p-2 rounded-xl text-white font-bold">Docs</div>
             </Link>
-            <Link href={'/sheets'}>
-                <div className="hover:bg-blue-500 p-2 rounded-xl bg-gray-200 text-black font-bold">Sheets</div>
+            <Link href={'/meet'}>
+                <div className="hover:bg-blue-500 p-2 rounded-xl bg-gray-200 text-black font-bold">Meet</div>
             </Link>
             <Link href={'/slides'}>
                 <div className="hover:bg-blue-500 p-2 rounded-xl bg-gray-200 text-black font-bold">Slides</div>

8f995d9527980409397aaf1836c6179c4c2360ea
massifcoder <vishalsharma243527@gmail.com>
2023-07-13 16:09:27 +0530

    googleAuths:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <link rel="icon" href="/home/sideImage.png" />
+        <title>Cloud Gallery</title>
+      </Head>
+    <Component {...pageProps} />
+    <Script src='https://accounts.google.com/gsi/client'/>
+    </>
 }
 
 export default MyApp

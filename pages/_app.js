dfc2896febeb6cea78e5e1ac72f0dd8251cc27b5
Vishal Sharma <massifcoder>
2023-11-26 04:05:22 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return <>
       <Head>
         <link rel="icon" href="/home/sideImage.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Nanda Docs</title>
       </Head>
     <Component {...pageProps} />
